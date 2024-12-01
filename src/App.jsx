0ecef44d3b24301a7bb1fc09d56497c2d2faa810
anugrahp13 <anugrahprastyo13@gmail.com>
2024-12-01 10:56:01 +0700

    feat[app.js]: changed source code for using theme dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+// Import React and useEffect
+import React, { useEffect } from "react";
+
 //import Toaster
 import { Toaster } from "react-hot-toast";
 
+// Import theme store
+import { useStore } from "./stores/theme";
+
 function App() {
+  // destructure state "theme" from useStore
+  const { theme } = useStore();
+
+  // set document theme
+  useEffect(() => {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+  }, [theme]);
+
   return (
     <>
       <Toaster />
